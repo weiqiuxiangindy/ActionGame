5d63d90b2f564d394526f7ce88b7450e2ab533cb
weiqiuxiangindy <weiqiuxiangindy@gmail.com>
2022-04-19 11:55:42 +0900

    add TextExtensions

---
+++ Assets/Scripts/Extensions/TextExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionGame.Extensions
+{
+    /// <summary>
+    /// テキストの拡張クラス
+    /// </summary>
+    public static class TextExtensions
+    {
+
+    }
+}
