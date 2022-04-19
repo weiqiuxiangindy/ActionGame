dc58b9a6fc27a163e31c7b6924274f56f7ef7f04
weiqiuxiangindy <weiqiuxiangindy@yahoo.co.jp>
2022-04-19 11:28:49 +0900

    upload project

---
+++ Assets/Scripts/Interfaces/IParam.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IParam
+{
+    int HP {get;}
+    bool IsJump {get;}
+}
---
+++ Assets/Scripts/Player/PlayerParam.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionGame.Player
+{
+    /// <summary>
+    /// プレイヤーのデータ
+    /// </summary>
+    public class PlayerParam : IParam
+    {
+        public int HP => throw new System.NotImplementedException();
+        public bool IsJump => throw new System.NotImplementedException();
+    }
+}
